f74ad440d52246eeb165df2fb7af1d5d8e763d67
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2023-04-04 13:32:55 +0900

    1차 제출전

--- 04_3차작업_aboutme를toss처럼/web_portfolio_4차_aboutme_토스처럼.js
+++ 04_3차작업_aboutme를toss처럼/web_portfolio_4차_aboutme_토스처럼.js
@@ -5,3 +5,36 @@
 
 // https://hello-5200.tistory.com/
 
+
+
+// 🟦 탭 기능 구현 
+
+  // 🔷 시도 1차 
+
+  // // let tabList = document.querySelectorAll('.tab_menu .list li');
+  // let buttonList = document.querySelectorAll('.nav-tabs button');
+  // for(let i = 0; i < buttonList.length; i++){
+  //     buttonList[i].addEventListener('click', function(e){
+  //     e.preventDefault();
+  //     for(let j = 0; j < buttonList.length; j++){
+  //         buttonList[j].classList.remove('active');
+  //     }
+  //     this.parentNode.classList.add('active');
+  //   });
+  // }
+
+  // 🔷 2차 
+
+  function openPopup() {
+    
+    let popup = document.querySelectorAll(".nav-item-wrap div"); 
+
+    console.log(popup)
+
+    if(popup.classList.contains("nav-item is-active")) {
+        popup.classList.remove("is-active")
+    } else {
+        popup.classList.add("is-active")
+    }
+
+  }
